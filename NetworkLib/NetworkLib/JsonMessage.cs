d4c0ef8a15fa982b5545862b9dde15740352eeb8
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-02-15 16:58:26 -0500

    * added json template header and data for every msg

---
+++ NetworkLib/NetworkLib/JsonMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkLib
+{
+    public class JsonMessage
+    {
+        public String header;
+        public String data;
+
+        public JsonMessage(String header, String data)
+        {
+            this.header = header;
+            this.data = data;
+        }
+    }
+}
---
+++ NetworkLib/NetworkLib/MessageSLTokens.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkLib
+{
+    public class MessageSLTokens
+    {
+        public String SL;
+        public List<BigInteger> tokens;
+        public List<BigInteger> exponents;
+
+        public MessageSLTokens(BigInteger SL, List<BigInteger> tokens, List<BigInteger> exponents)
+        {
+            this.SL = SL.ToString();
+            this.tokens = tokens;
+            this.exponents = exponents;
+        }
+    }
+}
