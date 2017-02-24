6079b91a087e742ccd1348d64bb97f98cd1b0afa
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-02-24 17:18:45 -0500

    * added message interface

---
+++ NetworkLib/NetworkLib/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkLib
+{
+    public interface IMessage
+    {
+        string Serialize<T>(T deserialized);
+        T Deserialize<T>(string serialized);
+    }
+}
