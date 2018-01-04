d2f431690846f7e755f961b94dc9a33e6e8e0281
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-04 23:55:54 +0100

    * added message module

---
+++ Common/Messages/Headers.cs
@@ -0,0 +1,7 @@
+namespace Common.Messages
+{
+    public static class Headers
+    {
+        public const string SR_SL_HEADER_REQ = "SR_SL_REQ";
+    }
+}
---
+++ Common/Messages/SlSrReq.cs
@@ -0,0 +1,8 @@
+namespace Common.Messages
+{
+    public class SlSrReq
+    {
+        const string HEADER = Common.Messages.Headers.SR_SL_HEADER_REQ;
+        string senderName;
+    }
+}
