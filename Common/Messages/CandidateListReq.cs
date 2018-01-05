ace94ef67cd53e9772ce99e1ac9a4bf519bf7d5b
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-05 15:05:22 +0100

    * added new message

---
+++ Common/Messages/CandidateListReq.cs
@@ -0,0 +1,9 @@
+namespace Common.Messages
+{
+    public class CandidateListReq
+    {
+        public string HEADER = Common.Messages.Headers.CANDIDATE_LIST_HEADER_REQ;
+        public string senderName;
+        public string slValue;
+    }
+}
