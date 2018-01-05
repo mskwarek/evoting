cf0fceac426cf39ac721361eeeb9508e33c4b37e
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-05 15:04:59 +0100

    * changed string format

--- Common/Messages/Headers.cs
+++ Common/Messages/Headers.cs
@@ -3,5 +3,6 @@ namespace Common.Messages
     public static class Headers
     {
         public const string SR_SL_HEADER_REQ = "SR_SL_REQ";
+        public const string CANDIDATE_LIST_HEADER_REQ = "CANDIDATE_LIST_REQ";
     }
 }
--- voterUnitTest/VoterToProxyMessagesUnitTest.cs
+++ voterUnitTest/VoterToProxyMessagesUnitTest.cs
@@ -3,6 +3,7 @@ using voter;
 using Moq;
 using Common;
 using Newtonsoft.Json;
+using System;
 
 namespace voterUnitTest
 {
@@ -11,11 +12,22 @@ namespace voterUnitTest
 
         Mock<IClient> transportLayerMock = new Mock<IClient>();
         Voter voter;
-        
+        string exampleName = "example";
+
         public VoterToProxyMessagesUnitTest()
         {
             voter = new Voter(transportLayerMock.Object);
         }
+
+        private void readVoterConfig()
+        {           
+            var configJson = String.Format("{{ \"name\" : \"{0}\" }}", exampleName);
+            Mock<IFileHelper> fileHelper = new Mock<IFileHelper>();
+
+            fileHelper.Setup(x => x.ReadAllText(It.IsAny<string>())).Returns(configJson);
+            voter.readConfiguration(fileHelper.Object, "someString");
+        }
+
         [Fact]
         public void requestForSrAndSlIsProperlySentWithoutNameCauseOfNoConfiguration()
         {    
@@ -28,17 +40,27 @@ namespace voterUnitTest
         [Fact]
         public void requestForSrAndSlIsProperlySentWithName()
         {
-            var exampleName = "example";
-            var configJson = "{ \"name\" : \""+exampleName+"\" }";
-            var expectedJson = "{\"HEADER\":\""+Common.Messages.Headers.SR_SL_HEADER_REQ+"\",\"senderName\":\""+exampleName+"\"}";
-            Mock<IFileHelper> fileHelper = new Mock<IFileHelper>();
-            fileHelper.Setup(x => x.ReadAllText(It.IsAny<string>())).Returns(configJson);
+            var expectedJson = String.Format("{{\"HEADER\":\"{0}\",\"senderName\":\"{1}\"}}", 
+                            Common.Messages.Headers.SR_SL_HEADER_REQ, exampleName);
 
-            voter.readConfiguration(fileHelper.Object, "someString");
+            readVoterConfig();
             voter.requestForSrAndSl();
 
             transportLayerMock.Verify(mock => mock.sendMessage(
                     It.Is<string>(s => s.Equals(expectedJson))), 
+                Times.Once());
+        }
+
+        [Fact]
+        public void requestForCandidateListIsProperlySent()
+        {
+            var expectedMessage = String.Format("{{\"HEADER\":\"{0}\",\"senderName\":\"{1}\",\"slValue\":\"{2}\"}}",
+                                    Common.Messages.Headers.CANDIDATE_LIST_HEADER_REQ, exampleName,
+                                    It.Is<string>(s => s.Equals("sadsa")));
+            // voter.requestForCandidateList();
+
+            transportLayerMock.Verify(mock => mock.sendMessage(
+                    It.Is<string>(s => s.Equals(expectedMessage))), 
                 Times.Once());
         }
     }
