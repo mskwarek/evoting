62c9d10e0d7d841679d2a00263bb01964c383657
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-07 23:19:07 +0100

    *added candidate list resp

---
+++ Common/Messages/CandidateListResp.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Common.Messages
+{
+    public class CandidateListResp
+    {
+        string HEDER;
+        string senderName;
+        List<string> candidateList;
+    }
+}
