71305c5edd10ca157b4659175b08772b7727118c
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-12 22:54:04 +0100

    * state pattern in progress

--- Common/State.cs
+++ Common/State.cs
@@ -1,7 +1,14 @@
+using Common.Messages;
+using System;
+
 namespace Common
 {
     public abstract class State
     {
+        public void handleMessage(IMessage message)
+        {
+            Common.Logger.log(String.Format("Unknown message received in this state {0}", message.getHeader()));
+        }
         public abstract void getNextState();
     }
 }
