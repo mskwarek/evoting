9d285f62e500ecd3a7c11c10396cc3dbe46bd97e
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-01-18 23:39:34 +0100

    * delegate added

--- Common/IClient.cs
+++ Common/IClient.cs
@@ -1,9 +1,12 @@
 namespace Common
 {
+    public delegate void NewMessageHandler(Common.Messages.IMessage message);
+
     public interface IClient
-    {
+    {    
         void sendMessage(string message);   
         void disconnect();
         bool connect(string ip, string port);
+        void subscribe(NewMessageHandler handler);
     }
 }
--- Common/State.cs
+++ Common/State.cs
@@ -5,6 +5,11 @@ namespace Common
 {
     public abstract class State
     {
+        public State(Common.IClient client)
+        {
+            client.subscribe(handleMessage);
+        }
+
         virtual public void handleMessage(IMessage message)
         {
             Common.Logger.log(String.Format("Unknown message received in this state {0}", message.getHeader()));
